5d5b8c7f04c254cc6bc82f50ac4d2f6016d1d3b1
chenfengbaobao <15538323378@163.com>
2018-02-01 17:37:50 +0800

    test

---
+++ src/helloword/Helloword.java
@@ -0,0 +1,17 @@
+package helloword;
+/**
+ *	@author:辰
+ *	@E-mail:15538323378@163.com
+ *   @weixin:wuhaifeng1613
+ *   @qq: 1648269848@qq.com
+ *	@2018年2月1日下午5:21:23
+ */
+public class Helloword {
+	public static void main(String[] args) {
+		
+		
+		System.out.println("hello word ! ");
+		
+	}
+
+}
