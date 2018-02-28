6375206eaa4b77c4796fe4c145df997e18f13abb
chenfengbaobao <15538323378@163.com>
2018-02-28 16:21:40 +0800

    commit src file

---
+++ src/dev/devT.java
@@ -0,0 +1,49 @@
+package dev;
+/**
+ *	@author:辰
+ *	@E-mail:15538323378@163.com
+ *	@CSDN博客：http://blog.csdn.net/chenfengbao
+ *   @weixin:wuhaifeng1613
+ *   @qq: 1648269848@qq.com
+ *	@2018年2月28日下午4:02:02
+ */
+public class devT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+
+	}
+
+}
